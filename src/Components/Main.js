bc1cb0deea7f43ddc0cb59b6500d5fe0d81f2e43
Gerzulet <german.alejandrozulet@gmail.com>
2022-12-10 18:25:36 -0300

    incorporacion animacion fade in

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -10,9 +10,11 @@ import Projects from './Projects'
 
 export default function Main () {
 
-const [section, setSection] = useState(<About/>)
+const [section, setSection] = useState(<Start/>)
 
-
+function cambiarDisplay (subject) {
+    setSection(subject)
+  }
 
 
   return (
@@ -20,14 +22,17 @@ const [section, setSection] = useState(<About/>)
       <main className="md:h-[44rem] w-full  relative">
         <div id="menu" className="w-[90%] md:w-[35%] h-[40rem] absolute left-5 md:left-4 top-7 ">
           <ul className="flex flex-col items-center mt-40 relative text-white text-4xl">
-            <li id="about"   data-text='&nbsp;SOBRE' className='my-4 cursor-pointer'  >&nbsp;Sobre&nbsp;</li>
-            <li id="contact" data-text='&nbsp;Contacto' className="my-4 cursor-pointer">&nbsp;Contacto&nbsp;</li>
-            <li id="skills" data-text='&nbsp;Competencias' className="my-4 cursor-pointer">&nbsp;Competencias&nbsp;</li>
-            <li id="projects" data-text='&nbsp;Proyectos' className="my-5 cursor-pointer ">&nbsp;Proyectos&nbsp;</li>
+            <li onClick={() => cambiarDisplay(<About/>)} id="about"   data-text='&nbsp;SOBRE' className='my-4 cursor-pointer'  >
+                &nbsp;Sobre&nbsp;
+
+            </li>
+            <li onClick={() => cambiarDisplay(<Contact/>)}  id="contact" data-text='&nbsp;Contacto' className="my-4 cursor-pointer">&nbsp;Contacto&nbsp;</li>
+            <li  onClick={() => cambiarDisplay(<Skills/>)}  id="skills" data-text='&nbsp;Competencias' className="my-4 cursor-pointer">&nbsp;Competencias&nbsp;</li>
+            <li onClick={() => cambiarDisplay(<Projects/>)}  id="projects" data-text='&nbsp;Proyectos' className="my-5 cursor-pointer ">&nbsp;Proyectos&nbsp;</li>
           </ul>
         </div>
         <div className="w-[60%] h-[40rem]  invisible md:visible absolute right-4 top-7 ">
-       <About/>
+          {section}
         </div>
 
       </main>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,15 @@ module.exports = {
     extend: {
       fontFamily: {
         inconsolata: ['"Inconsolata"' ]
+      },
+      animation : {
+           'fadein' : 'fadein 1.9s cubic-bezier(0.390, 0.575, 0.565, 1.000) both'
+      },
+      keyframes: {
+        fadein: {
+        '0%' : { opacity: 0 },
+        '100%' : {opacity: 1},
+        }
       }
     },
   },
