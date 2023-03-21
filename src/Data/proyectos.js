8cb6e8b773c49b8fbc9504ceff6354b8e775192f
Gerzulet <german.alejandrozulet@gmail.com>
2023-03-20 21:25:07 -0300

    [EDIT] Update Links

--- src/Data/proyectos.js
+++ src/Data/proyectos.js
@@ -1,30 +1,38 @@
 import noteAppPreview from '../Assets/notetakingappPreview.gif'
 import landingPagePreview from '../Assets/landingPagePreview.gif'
+import catWorking from '../Assets/catWorking.jpg'
 import tailwindIcon from '../Assets/tailwindLogo.svg'
 import reactIcon from '../Assets/reactLogo.svg'
 import jsLogo from '../Assets/jslogo.svg'
 import bootstrapLogo from '../Assets/bootstrapLogo.svg'
+
+
 const ProyectosDB = [
-    {
-      'image': landingPagePreview,
-      'technologies': [reactIcon, tailwindIcon],
-      'linkGithub' : 'https://github.com/Gerzulet/landing-page',
-      'responsive' : true, 
-      'ProyectTitle' : 'Landing Page',
-    },
-    {
-      'image': 'https://thumbs.dreamstime.com/b/polar-bear-ursus-maritimus-carnivorous-bear-whose-native-range-lies-largely-arctic-circle-encompassing-115252759.jpg',
-      'technologies': ['../Assets/tailwindLogo.svg', '../Assets/reactLogo.svg']
-    },
-    {
-      'image': noteAppPreview,
-      'ProyectTitle': 'Note Taking App',
-      'link': 'https://github.com',
-      'responsive': false,
-      'technologies': [jsLogo, bootstrapLogo],
-      'linkGithub': 'https://github.com/Gerzulet/NotesManager.github.io',
-      'linkPreview': 'https://gerzulet.github.io/NotesManager.github.io/'
-    }
-  ]
+  {
+    'image': landingPagePreview,
+    'technologies': [reactIcon, tailwindIcon],
+    'linkGithub': 'https://github.com/Gerzulet/landing-page',
+    'responsive': true,
+    'ProyectTitle': 'Landing Page',
+    'linkPreview': 'https://zulet-landingpage.netlify.app/'
+
+  },
+  {
+    'image': catWorking,
+    'technologies': [reactIcon, tailwindIcon],
+    'linkGithub': 'https://github.com/Gerzulet/ProyectoFinalSoto',
+    'responsive': true,
+    'ProyectTitle': 'E-Commerce (WORK IN PROGRESS)',
+
+  },
+  {
+    'image': noteAppPreview,
+    'ProyectTitle': 'Note Taking App',
+    'responsive': false,
+    'technologies': [jsLogo, bootstrapLogo],
+    'linkGithub': 'https://github.com/Gerzulet/NotesManager.github.io',
+    'linkPreview': 'https://gerzulet.github.io/NotesManager.github.io/'
+  }
+]
 
 export default ProyectosDB
